7d743fb5e396d15226203b9366b1bdf2c73d949b
Paavo Pärssinen <paavo.parssinen@nokia.com>
2019-11-07 15:01:45 +0000

    Solve 1.7 and 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,28 +9,35 @@ const App = () => {
 
   return (
     <div>
-      <Header header="Give feedback"></Header>
+      <Header header="Give feedback" />
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics good={good} bad={bad} neutral={neutral}></Statistics>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
 
-const Header = ({header}) => {
-    return <h1>{header}</h1>;
-  };
+const Header = ({ header }) => {
+  return <h1>{header}</h1>;
+};
 
-const Statistics = ({good, neutral, bad}) => {
-    return (
-        <div>
-            <Header header="Statistics"></Header>
-            <div>Good: {good}</div>
-            <div>Neutral: {neutral}</div>
-            <div>Bad: {bad}</div>
-        </div>
-    )
-}
+const Statistics = ({ good, neutral, bad }) => {
+  const total = bad + neutral + good;
+  const average = (good - bad) / total;
+  const posp = ((good / total) * 100).toFixed(2) + " %";
+
+  return (
+    <div>
+      <Header header="Statistics" />
+      <div>Good: {good}</div>
+      <div>Neutral: {neutral}</div>
+      <div>Bad: {bad}</div>
+      <div>All: {total}</div>
+      <div>Average: {average}</div>
+      <div>Positive: {posp}</div>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
