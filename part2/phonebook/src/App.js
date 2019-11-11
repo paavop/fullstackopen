ff98e4a4f820e49011a064ced92a4ca7329d1d76
Paavo Pärssinen <paavo.parssinen@nokia.com>
2019-11-11 10:02:49 +0000

    Solve 2.17

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -32,7 +32,7 @@ const App = () => {
         setPersons={setPersons}
       />
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} />
+      <Persons persons={persons} filter={filter} setPersons={setPersons}/>
     </div>
   );
 };
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,17 +1,29 @@
 import React from "react";
-import Person from "./Person"
+import Person from "./Person";
+import personService from "../services/persons";
 
-const Persons = ({ persons, filter }) => {
+const Persons = ({ persons, filter, setPersons }) => {
   const rows = () =>
     persons
       .filter(person =>
         person.name.toLowerCase().includes(filter.toLowerCase())
       )
-      .map(p => <Person key={p.name} person={p} />);
-
-    return(
-        <div>{rows()}</div> 
-    )
+      .map(p => (
+        <Person
+          key={p.name}
+          person={p}
+          deletePerson={() => deletePerson(p.id)}
+        />
+      ));
+  const deletePerson = id => {
+    const name = persons.find(p => p.id === id).name;
+    if (window.confirm(`Delete ${name}?`)) {
+      personService.remove(id).then(() => {
+        setPersons(persons.filter(p => p.id !== id));
+      });
+    }
+  };
+  return <div>{rows()}</div>;
 };
 
 export default Persons;
