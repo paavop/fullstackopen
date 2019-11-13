a2972c9beae639c3adf9df60c2fd6e98a5f38ca9
Paavo Pärssinen <paavo.parssinen@nokia.com>
2019-11-13 13:16:48 +0000

    Solve 3.5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 let persons = [
   {
@@ -23,6 +26,11 @@ let persons = [
     id: 4
   }
 ];
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0;
+  return maxId + 1;
+};
 
 app.get("/info", (req, res) => {
   const time = new Date();
@@ -50,6 +58,25 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter(person => person.id !== id);
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name) {
+    return response.status(400).json({
+      error: "content missing"
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const port = 3001;
