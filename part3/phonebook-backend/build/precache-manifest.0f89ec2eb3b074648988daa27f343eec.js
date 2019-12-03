b9f3a25236802d256f215769b8328e50fd9463b6
Paavo Pärssinen <paavo.parssinen@aalto.fi>
2019-12-03 11:06:00 +0200

    Add frontend production build to heroku

---
+++ part3/phonebook-backend/build/precache-manifest.0f89ec2eb3b074648988daa27f343eec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee3cae29221c71a8d40c0132c0e4b46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a530f521091137132ce1",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9cabba44ff9439239689",
+    "url": "/static/js/2.3e63fad9.chunk.js"
+  },
+  {
+    "revision": "a530f521091137132ce1",
+    "url": "/static/js/main.68a753d7.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
