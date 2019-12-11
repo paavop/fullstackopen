d8ae19a1f18852721f457bb2bdb5af9886ba1f99
Paavo Pärssinen <paavo.parssinen@aalto.fi>
2019-12-11 08:59:43 +0200

    Use phonebook database

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require('cors');
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 
@@ -23,53 +25,36 @@ app.use(
 );
 app.use(cors());
 
-let persons = [{
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }
-];
-
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.get("/info", (req, res) => {
-  const time = new Date();
-  res.send(
-    `<div>Phonebook has info for ${persons.length} people<br>${time}</div>`
-  );
+  Person.find({}).then(people => {
+    const time = new Date();
+    res.send(
+      `<div>Phonebook has info for ${people.length} people<br>${time}</div>`
+    );
+  })
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  console.log(request.params.id)
+  Person.findById(request.params.id).then(person => {
+      if (person) {
+        response.json(person.toJSON())
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(400).send({
+        error: 'malformatted id'
+      })
+    })
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -85,24 +70,19 @@ app.post("/api/persons", (request, response) => {
       error: "content missing"
     });
   }
-  if (persons.filter(person => person.name === body.name).length) {
-    return response.status(400).json({
-      error: "Name is already in phonebook"
-    });
-  }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId()
-  };
+  })
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then(response => {
+    console.log('person saved!')
+    response.json(person.toJSON());
+  })
 });
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
